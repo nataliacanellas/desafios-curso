6a48ad61237e0af79f24f7d5074ef93056533b6d
Natalia Canellas <nataliafcanellas@gmail.com>
2022-07-01 10:45:14 -0300

    [up] Updated the file.

--- operadores.dart
+++ operadores.dart
@@ -1,24 +1,24 @@
 void main() {
   double nota1 = 10;
   double nota2 = 7.5;
-  print(nota1 + nota2);
-  print(nota1 - nota2);
+  // print(nota1 + nota2);
+  // print(nota1 - nota2);
   // print(nota1 = -nota2);
-  print(nota1 * nota2);
-  print(nota1 / nota2);
+  // print(nota1 * nota2);
+  // print(nota1 / nota2);
 
   //Essa próxima divisão é para retornar apenas a parte inteira do resultado.
-  print(nota1 ~/ nota2);
+  // print(nota1 ~/ nota2);
   //O próximo operador % é para pegar o resto da divisão,
   // muito útil para achar números pares ou impares.
-  print(nota1 % nota2);
+  // print(nota1 % nota2);
   //Operadores lógicos
-  print(nota1 > nota2); //Sinal de maior que
-  print(nota1 >= nota2); //True or false = true
-  print(nota1 < nota2); //Sinal de menor que
-  print(nota1 <= nota2); // menor ou igual
-  print(nota1 == nota2); // igual a
-  print(nota1 != nota2); // diferente
+  // print(nota1 > nota2); //Sinal de maior que
+  // print(nota1 >= nota2); //True or false = true
+  // print(nota1 < nota2); //Sinal de menor que
+  // print(nota1 <= nota2); // menor ou igual
+  // print(nota1 == nota2); // igual a
+  // print(nota1 != nota2); // diferente
 
   //Operadores de atibuição
   dynamic nota3 = 25;
@@ -29,5 +29,17 @@ void main() {
   nota3 = (nota3 ~/ 2).toDouble(); //modo mais feio
   nota3 ~/= 2; //modo mais curto mas tivemos que mudar a variável para dynamic
   //pois antes deu erro pois o resultado daria int e a variável era double.
-  print(nota3);
+
+  // print(nota3 is double); //pergunta se a nota3 é double
+  // print(nota1 is! int); //pergunta se a nota1 não é int
+  // print(nota1.runtimeType); //diz o tipo da variável
+
+  //Praticando mais
+  String game = 'The Sims';
+  int version = 4;
+  dynamic value = 10;
+  print(version.runtimeType);
+  print(game is bool);
+  print(version is! double);
+  print(value is int);
 }
